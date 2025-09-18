b137c18a6a5576e368b4aa9033f7c1bb4b87077a
Jakub Mroz <kuba.mroz01@gmail.com>
2025-09-18 11:26:28 +0200

    fix: menuitem conditional render

--- src/components/menu/menuItem/index.js
+++ src/components/menu/menuItem/index.js
@@ -4,21 +4,21 @@ import ArrowRightIcon from '../../../assets/icons/arrowRightIcon';
 const MenuItem = ({ icon, text, children, linkTo = '#nogo', onClick }) => {
   return (
     <li>
-  {linkTo === '#nogo' ? (
-    <button onClick={onClick}>
-      {icon}
-      <p>{text}</p>
-      {children && <ArrowRightIcon />}
-    </button>
-  ) : (
-    <NavLink to={linkTo}>
-      {icon}
-      <p>{text}</p>
-      {children && <ArrowRightIcon />}
-    </NavLink>
-  )}
-  {children && <ul>{children}</ul>}
-</li>
+      {linkTo === '#nogo' && onClick != null ? (
+        <button onClick={onClick}>
+          {icon}
+          <p>{text}</p>
+          {children && <ArrowRightIcon />}
+        </button>
+      ) : (
+        <NavLink to={linkTo}>
+          {icon}
+          <p>{text}</p>
+          {children && <ArrowRightIcon />}
+        </NavLink>
+      )}
+      {children && <ul>{children}</ul>}
+    </li>
   );
 };
 
