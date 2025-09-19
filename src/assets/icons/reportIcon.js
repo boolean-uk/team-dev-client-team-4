fff82f87fe468862a374b7b5722381788cc8f8df
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-09-19 11:28:13 +0200

    fix: better report icon

--- src/assets/icons/reportIcon.js
+++ src/assets/icons/reportIcon.js
@@ -1,14 +1,14 @@
 const ReportIcon = () => {
   return (
-    <svg width="32" height="36" viewBox="0 0 32 36" xmlns="http://www.w3.org/2000/svg">
+    <svg width="36" height="36" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
       <polygon
-        points="16,2 28,10 28,26 16,34 4,26 4,10"
+        points="16,2 32,2 46,16 46,32 32,46 16,46 2,32 2,16"
         fill="white"
         stroke="black"
-        strokeWidth="2"
+        strokeWidth="3"
       />
-      <rect x="14" y="10" width="4" height="12" fill="black" />
-      <rect x="14" y="24" width="4" height="4" fill="black" />
+      <rect x="22" y="14" width="4" height="16" fill="black" />
+      <rect x="22" y="32" width="4" height="4" fill="black" />
     </svg>
   );
 };
