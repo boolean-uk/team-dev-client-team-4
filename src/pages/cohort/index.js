c64dec986f28349b85e875df1c4f51c8fb9f41d8
Trym Haugan Berger <trym.haugan.berger@experisacademy.no>
2025-09-23 12:32:05 +0200

    completed cohort page setup and navigation

---
+++ src/pages/cohort/index.js
@@ -0,0 +1,11 @@
+
+const CohortPage = () => {
+  return (
+        <div>
+            <h1>Cohort Page</h1>
+            <p>Welcome to your Cohort page.</p>
+        </div>
+  );
+};
+
+export default CohortPage;
