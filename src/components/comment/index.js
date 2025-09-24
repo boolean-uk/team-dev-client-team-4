7ea61a3984fbd1a286d51fae27d2628632465c1e
elisabethroys <125894666+elisabethroys@users.noreply.github.com>
2025-09-24 11:20:07 +0200

    New css fix

--- src/components/comment/index.js
+++ src/components/comment/index.js
@@ -5,7 +5,7 @@ const Comment = ({ name, content, editedComment }) => {
     <div>
       <div className={`comment-details ${!editedComment ? 'single-column' : ''}`}>
         <h5>{name}</h5>
-        <h5 className='edit-tag'> { editedComment && 'Edited' } </h5>
+        <h5 className='edit-tag-comment'> { editedComment && 'Edited' } </h5>
       </div>
       <p>{content}</p>
     </div>
