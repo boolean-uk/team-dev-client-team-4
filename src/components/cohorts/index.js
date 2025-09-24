77f0f6a775bec68a3bb3696c440711215b88f9f7
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-09-24 13:44:13 +0200

    fix: color change on cohorts

--- src/components/cohorts/index.js
+++ src/components/cohorts/index.js
@@ -2,12 +2,12 @@ import { useEffect, useState } from 'react';
 import './style.css';
 import { API_URL } from '../../service/constants';
 import BicodeIcon from '../../assets/icons/bicodeIcon';
-import ProfileIcon from '../../assets/icons/profileIcon';
 
 const Cohorts = () => {
   const [cohorts, setCohorts] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  // TODO: switch to new endpoint when available
   useEffect(() => {
     setLoading(true);
     fetch(`${API_URL}/cohorts`)
@@ -34,7 +34,7 @@ const Cohorts = () => {
             cohorts.map((cohort) => {
               return (
                     <li key={cohort.id} className="cohorts-list-item">
-                        <div className="profile-icon" style={{ background: '#05f569ff' }}>
+                        <div className="profile-icon" style={{ background: '#1fd76cff' }}>
                             <BicodeIcon />
                         </div>
                     </li>
