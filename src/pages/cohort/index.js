83745129df67ba6859e97a61f1334c4c4440ca37
Mona Eikli Andresen <monaeik@hotmail.com>
2025-09-25 15:57:28 +0200

    Added Cohort- and StudentList for Teachers. And did some small fixes that needed to be done

--- src/pages/cohort/index.js
+++ src/pages/cohort/index.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 import { API_URL } from '../../service/constants';
 import StudentCohortTeachersList from '../../components/StudentCohortTeachersList';
+import CohortStudentListForTeacher from '../../components/CohortStudentListForTeacher';
 import MyCohortCard from '../../components/myCohortPage';
 import MyExercises from '../../components/MyExercises';
 import Card from '../../components/card';
@@ -63,6 +64,12 @@ const CohortPage = () => {
           <MyExercises />
         </Card>
       </aside>
+      )}
+
+      {userRole === 'Teacher' && (
+        <main className="cohort-teacher-main">
+          <CohortStudentListForTeacher userId={userId} />
+        </main>
       )}
     </>
   );
