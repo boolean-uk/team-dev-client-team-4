574e141bb62a3fed811fe5c860888368d33ec8f5
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-09-25 15:30:16 +0200

    feat: arrowBackIcon onclick navigate back + template for search results

--- src/pages/searching/index.js
+++ src/pages/searching/index.js
@@ -1,11 +1,26 @@
+import Card from '../../components/card';
+import SearchResults from '../../components/searchResults';
+import ArrowBackIcon from '../../assets/icons/arrowBackIcon';
+import './searching.css';
+import { useNavigate } from 'react-router-dom';
+
 const Searching = () => {
-  console.log('Searching component loaded');
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <>
-      <main>
-        <p>Not implemented...</p>
-      </main>
-    </>
+    <main className="search-page">
+      <div className="search-header">
+        <ArrowBackIcon className="back-icon" onClick={goBack} />
+        <h3>Search Results</h3>
+      </div>
+      <Card>
+        <SearchResults />
+      </Card>
+    </main>
   );
 };
 
