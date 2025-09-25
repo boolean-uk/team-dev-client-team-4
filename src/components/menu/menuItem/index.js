cbe7ef9db4ad3ee2f672c63fc299a62aec68dad3
Jakub Mroz <kuba.mroz01@gmail.com>
2025-09-25 13:00:49 +0200

    feat: cascading menu opens to the side

--- src/components/menu/menuItem/index.js
+++ src/components/menu/menuItem/index.js
@@ -1,9 +1,27 @@
 import { NavLink } from 'react-router-dom';
 import ArrowRightIcon from '../../../assets/icons/arrowRightIcon';
+import { useEffect, useRef, useState } from 'react';
 
 const MenuItem = ({ icon, text, children, linkTo = '#nogo', onClick }) => {
+  const itemRef = useRef(null);
+  const [openLeft, setOpenLeft] = useState(false);
+
+  const breakPxLimit = 200; // change this to decide how fast the cascading menu changes to other side
+
+  useEffect(() => {
+    if (children && itemRef.current) {
+      const rect = itemRef.current.getBoundingClientRect();
+      const submenuWidth = breakPxLimit;
+      const spaceRight = window.innerWidth - rect.right;
+
+      if (spaceRight < submenuWidth) {
+        setOpenLeft(true);
+      }
+    }
+  }, [children]);
+
   return (
-    <li>
+    <li ref={itemRef} className={openLeft ? 'open-left' : ''}>
       {linkTo === '#nogo' && onClick != null ? (
         <button onClick={onClick}>
           {icon}
